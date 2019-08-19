bb2c929d5b4b0ca6bf915851a6d5ff18bcd80dcb
joao.freitas <joao.frhb@gmail.com>
2019-08-18 22:10:46 -0300

    projeto completo

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'; 
+
+const Header = () => (
+    <header id="main-header"><a href="/">JSHunt</a></header>
+);
+
+//  A mesma Coisa
+// class Header extends Component{
+//     render(){
+//         <header id="main-header">JSHunt</header>
+//     }
+// }
+
+export default Header;
